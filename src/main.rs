a26fac18e8e5403d469289433fa1b774379a5bb3
Denis Fomin <fo.denis2011@gmail.com>
2026-01-08 17:36:04 +0700

    make login more reliable

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use std::path::PathBuf;
 
 use gpui::*;
-use gpui_component::{Root, Theme, ThemeRegistry};
+use gpui_component::{Root, Theme, ThemeRegistry, WindowExt};
 
-use anyhow::{Result as AResult};
-use rpc::client::Connection;
+use anyhow::Result as AResult;
+use rpc::{
+    client::Connection,
+    models::auth::{LoginError, LoginPayload, SessionKey},
+};
 
-pub mod db;
 pub mod assets;
+pub mod db;
 pub mod gpui_tokio;
 pub mod screens;
 
 use screens::login::LoginScreen;
 
-use crate::{assets::Assets, db::{DBConnectionManager, entity::registry}, gpui_tokio::Tokio};
+use crate::{assets::Assets, db::DBConnectionManager, gpui_tokio::Tokio};
 
 enum Screen {
     Login(Entity<LoginScreen>),
@@ -46,14 +49,6 @@ impl ConnectionManger {
             .unwrap()
     }
 
-    async fn login(cx: &mut AsyncApp) {
-        Tokio::spawn(cx, async move {
-        })
-            .unwrap()
-            .await
-            .unwrap();
-    }
-
     async fn connect(cx: &mut AsyncApp, server_ip: String) -> AResult<()> {
         let connected = cx.read_global(|g: &Self, _| g.is_connected())?;
 
@@ -76,14 +71,16 @@ impl Global for ConnectionManger {}
 
 impl Render for MainWindow {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
-        let screen = match &self.current_screen {
-            Screen::Login(screen) => screen.clone(),
-            _ => todo!(),
-        };
-
         let notification_layer = Root::render_notification_layer(window, cx);
 
-        div().size_full().child(screen).children(notification_layer)
+        let mut root = div().size_full();
+
+        match &self.current_screen {
+            Screen::Login(screen) => root = root.child(screen.clone()),
+            Screen::MainWorkspace => root = root.child("WORKSPACE"),
+        };
+
+        root.children(notification_layer)
     }
 }
 
@@ -113,35 +110,103 @@ fn main() {
 
         // Check if we're already authorized
         cx.spawn(async move |cx| {
-            db::init(cx).await
-                .unwrap();
+            db::init(cx).await.unwrap();
 
             let db = DBConnectionManager::get(cx);
-            let registry = Tokio::spawn(cx, async move {
-                DBConnectionManager::get_registry(&db).await
-            })?.await?;
-
-            // Open the window first to not block it if the server
-            // is slow to response on login
-            cx.open_window(WindowOptions::default(), |window, cx| {
-                let login_screen = cx.new(|cx| LoginScreen::new(
-                    window,
+            let registry =
+                Tokio::spawn(
                     cx,
-                    registry.session_key.is_some(),
-                    registry.connected_server,
-                ));
-                let view = cx.new(|_| MainWindow {
-                    current_screen: Screen::Login(login_screen),
+                    async move { DBConnectionManager::get_registry(&db).await },
+                )?
+                .await?;
+
+            cx.open_window(WindowOptions::default(), |window, cx| {
+                let login_screen = cx.new(|cx| {
+                    LoginScreen::new(
+                        window,
+                        cx,
+                        registry.session_key.is_some(),
+                        registry.connected_server.clone(),
+                    )
                 });
+
+                let view = cx.new(|cx| {
+                    cx.subscribe(&login_screen, |this: &mut MainWindow, _, _: &(), _| {
+                        this.current_screen = Screen::MainWorkspace;
+                    })
+                    .detach();
+
+                    MainWindow {
+                        current_screen: Screen::Login(login_screen.clone()),
+                    }
+                });
+
+                let (tx, rx) = smol::channel::bounded::<String>(1);
+
+                window.spawn(cx, async move |window| {
+                    let message = rx.recv().await?;
+
+                    window.update(|window, cx| {
+                        window.push_notification(message, cx);
+                    }).ok();
+
+                    Ok::<_, anyhow::Error>(())
+                }).detach();
+
+                cx.spawn({
+                    let view = view.clone();
+
+                    async move |cx| {
+                        if let (Some(session_key), Some(server_ip)) = (registry.session_key, registry.connected_server) {
+                            if ConnectionManger::connect(cx, server_ip.clone()).await.is_err() {
+                                // TODO: That's not how it works unfortunately, change it.
+                                // ConnectionManger would try to connect infinitely and will never
+                                // time out
+                                tx.send(format!("failed to connect to: {server_ip}")).await.ok();
+                                
+                                return;
+                            }
+                    
+                            let connection = ConnectionManger::get(cx);
+
+                            match rmp_serde::from_slice::<SessionKey>(&session_key) {
+                                Ok(session_key) => {
+                                    let result: Result<(), LoginError> = connection
+                                        .execute("Login", &LoginPayload { session_key })
+                                        .await
+                                        .expect("invalid params");
+
+                                    if result.is_ok() {
+                                        view.update(cx, |this, _| {
+                                            this.current_screen = Screen::MainWorkspace;
+                                        }).ok();
+                                    } else {
+                                        login_screen.update(cx, |this, _| {
+                                            this.is_connecting = false;
+                                        }).ok();
+
+                                        tx.send("Stale session, please log in".into())
+                                            .await.ok();
+                                    }
+                                }
+                                Err(_) => {
+                                    login_screen.update(cx, |this, _| {
+                                        this.is_connecting = false;
+                                    }).ok();
+
+                                    tx.send("Corrupted data, please log in again".into())
+                                        .await.ok();
+                                }
+                            };
+                        }
+                    }
+                }).detach();
 
                 // For notifications and stuff, this should be the first
                 // element of the window (aka root)
                 cx.new(|cx| Root::new(view, window, cx))
-            }).unwrap();
-
-            if let Some(_session_key) = registry.session_key {
-                todo!("Implement Login");
-            }
+            })
+            .unwrap();
 
             Ok::<_, anyhow::Error>(())
         })
