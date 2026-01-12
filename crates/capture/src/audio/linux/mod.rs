1fad3ca467308a322bf001a0a5567d26173a66be
Denis Fomin <fo.denis2011@gmail.com>
2026-01-12 11:40:18 +0700

    (feat) add basic audio capture

--- crates/capture/src/audio/linux/mod.rs
+++ crates/capture/src/audio/linux/mod.rs
@@ -1,4 +1,13 @@
-use pipewire as pw;
+use libspa::param::{
+    ParamType,
+    audio::{AudioFormat, AudioInfoRaw},
+    format::{MediaSubtype, MediaType},
+    format_utils,
+};
+use pipewire::{
+    self as pw,
+    stream::{Stream, StreamBox, StreamListener},
+};
 use pw::{properties::properties, spa};
 use spa::pod::Pod;
 
@@ -12,8 +21,154 @@ pub const CHAN_SIZE: usize = std::mem::size_of::<i16>();
 
 pub struct Audio {}
 
+/// Shared data between events
+struct CaptureStreamData {
+    format: AudioInfoRaw,
+}
+
+struct CaptureStream<'a> {
+    stream: StreamBox<'a>,
+    stream_listener: StreamListener<CaptureStreamData>,
+}
+
+impl<'a> CaptureStream<'a> {
+    const STREAM_NAME: &'static str = "HAZEL Audio Capture";
+
+    /// Gets called when the stream param changes.
+    /// We're only looking for format changes
+    fn on_param_change(
+        _stream: &Stream,
+        user_data: &mut CaptureStreamData,
+        id: u32,
+        param: Option<&libspa::pod::Pod>,
+    ) {
+        // NULL means to clear the format
+        let Some(param) = param else {
+            return;
+        };
+
+        if param.is_none() || id != ParamType::Format.as_raw() {
+            return;
+        }
+
+        let (media_type, media_subtype) = match format_utils::parse_format(param) {
+            Ok(v) => v,
+            Err(_) => return,
+        };
+
+        // Only accept raw audio
+        if media_type != MediaType::Audio || media_subtype != MediaSubtype::Raw {
+            return;
+        }
+
+        user_data
+            .format
+            .parse(param)
+            .expect("Failed to parse param changed to AudioInfoRaw");
+
+        println!(
+            "capturing rate:{} channels:{}",
+            user_data.format.rate(),
+            user_data.format.channels()
+        );
+    }
+
+    /// Gets called when we can take the data
+    fn on_process(stream: &Stream, user_data: &mut CaptureStreamData) {
+        let buffer = stream.dequeue_buffer();
+
+        let Some(mut buffer) = buffer else {
+            println!("Out of buffers to capture");
+
+            return;
+        };
+
+        let datas = buffer.datas_mut();
+        if datas.is_empty() {
+            return;
+        }
+
+        let data = &mut datas[0];
+
+        let n_channels = user_data.format.channels();
+        let n_samples = data.chunk().size() / (std::mem::size_of::<f32>() as u32);
+
+        let Some(samples) = data.data() else {
+            return;
+        };
+
+        for c in 0..n_channels {
+            let mut max: f32 = 0.0;
+
+            for n in (c..n_samples).step_by(n_channels as usize) {
+                let start = n as usize * std::mem::size_of::<f32>();
+                let end = start + std::mem::size_of::<f32>();
+
+                let chan = &samples[start..end];
+                let f = f32::from_le_bytes(chan.try_into().unwrap());
+
+                max = max.max(f.abs());
+            }
+
+            let peak = ((max * 30.0) as usize).clamp(0, 39);
+        }
+    }
+
+    fn new(core: &'a pw::core::CoreRc) -> AResult<Self> {
+        let capture_stream = pw::stream::StreamBox::new(
+            core,
+            Self::STREAM_NAME,
+            properties! {
+                *pw::keys::MEDIA_TYPE => "Audio",
+                *pw::keys::MEDIA_ROLE => "Communication",
+                *pw::keys::MEDIA_CATEGORY => "Capture",
+            },
+        )?;
+
+        let mut audio_info = spa::param::audio::AudioInfoRaw::new();
+        audio_info.set_format(AudioFormat::F32LE);
+
+        let values: Vec<u8> = pw::spa::pod::serialize::PodSerializer::serialize(
+            std::io::Cursor::new(Vec::new()),
+            &pw::spa::pod::Value::Object(pw::spa::pod::Object {
+                type_: libspa::sys::SPA_TYPE_OBJECT_Format,
+                id: libspa::sys::SPA_PARAM_EnumFormat,
+                properties: audio_info.into(),
+            }),
+        )
+        .unwrap()
+        .0
+        .into_inner();
+
+        let mut params = [Pod::from_bytes(&values).unwrap()];
+        let stream_data = CaptureStreamData {
+            format: Default::default(),
+        };
+
+        let listener = capture_stream
+            .add_local_listener_with_user_data(stream_data)
+            .process(CaptureStream::on_process)
+            .param_changed(CaptureStream::on_param_change)
+            .register()?;
+
+        capture_stream.connect(
+            spa::utils::Direction::Input,
+            None,
+            pw::stream::StreamFlags::AUTOCONNECT
+                | pw::stream::StreamFlags::MAP_BUFFERS
+                | pw::stream::StreamFlags::RT_PROCESS,
+            &mut params,
+        )?;
+
+        Ok(Self {
+            stream: capture_stream,
+            stream_listener: listener,
+        })
+    }
+}
+
 impl Audio {
-    const AUDIO_STREAM_NAME: &'static str = "HAZEL";
+    const PLAYBACK_STREAM_NAME: &'static str = "HAZEL Audio Playback";
 
     pub fn new() -> AResult<Self> {
         pw::init();
@@ -25,12 +180,12 @@ impl Audio {
         let context = pw::context::ContextRc::new(&mainloop, None)?;
         let core = context.connect_rc(None)?;
 
-        let stream = pw::stream::StreamBox::new(
+        let playback_stream = pw::stream::StreamBox::new(
             &core,
-            Self::AUDIO_STREAM_NAME,
+            Self::PLAYBACK_STREAM_NAME,
             properties! {
                 *pw::keys::MEDIA_TYPE => "Audio",
-                *pw::keys::MEDIA_ROLE => "Music",
+                *pw::keys::MEDIA_ROLE => "Communication",
                 *pw::keys::MEDIA_CATEGORY => "Playback",
                 *pw::keys::AUDIO_CHANNELS => "2",
             },
@@ -47,7 +202,7 @@ impl Audio {
         audio_info.set_position(position);
 
         let data: f64 = 0.0;
-        let _listener = stream
+        let _listener = playback_stream
             .add_local_listener_with_user_data(data)
             .process(|stream, acc| match stream.dequeue_buffer() {
                 None => println!("No buffer received"),
@@ -96,7 +251,7 @@ impl Audio {
 
         let mut params = [Pod::from_bytes(&values).unwrap()];
 
-        stream.connect(
+        playback_stream.connect(
             spa::utils::Direction::Output,
             None,
             pw::stream::StreamFlags::AUTOCONNECT
@@ -104,6 +259,8 @@ impl Audio {
                 | pw::stream::StreamFlags::RT_PROCESS,
             &mut params,
         )?;
+
+        let capture_stream = CaptureStream::new(&core)?;
 
         mainloop.run();
 
