dc15d4980a8886c8de8c4f7de9d1a0fe304f7bf8
Denis Fomin <fo.denis2011@gmail.com>
2026-01-18 13:43:36 +0700

    feat: migrate  on new rpc API

--- crates/server/src/api/auth.rs
+++ crates/server/src/api/auth.rs
@@ -1,19 +1,20 @@
 use rpc::{
-    common::Empty,
     models::{
         auth::{
-            GetCurrentUser, GetCurrentUserError, GetSessionKeyError, GetSessionKeyPayload,
-            GetSessionKeyResponse, Login, LoginError, LoginPayload, SessionKey,
+            GetCurrentUser, GetSessionKey, GetSessionKeyError,
+            GetSessionKeyPayload, GetSessionKeyResponse, Login, LoginError, LoginPayload,
+            SessionKey,
         },
         common::{APIError, RPCMethod as _},
         markers::TaggedEntity,
     },
-    server::RpcRouter,
 };
 
 use sha2::{Digest, Sha256};
 
-use crate::{AppState, ConnectionState, api::common::{DbErrReponseCompat as _, RPCServer}};
+use crate::{
+    AppState, ConnectionState, GlobalRouter, api::common::{DbErrReponseCompat as _, RPCServer}
+};
 use crate::{
     entity::user::{self, Entity as User},
     register_endpoints,
@@ -23,54 +24,48 @@ use sea_orm::{DbErr, entity::*, query::*};
 
 const KEY: &[u8] = b"TODO";
 
-async fn get_session_key(
-    state: AppState,
-    _conn_state: ConnectionState,
-    GetSessionKeyPayload { login, password }: GetSessionKeyPayload,
-) -> Result<GetSessionKeyResponse, GetSessionKeyError> {
-    let password = Sha256::digest(password.as_bytes());
-    let password = format!("{:x}", password);
+impl RPCServer for GetSessionKey {
+    async fn handle(
+        app_state: AppState,
+        _connection_state: ConnectionState,
+        GetSessionKeyPayload { login, password }: GetSessionKeyPayload,
+    ) -> Self::Response {
+        let password = Sha256::digest(password.as_bytes());
+        let password = format!("{:x}", password);
 
-    let user = User::find()
-        .filter(user::Column::Username.eq(&login))
-        .one(&state.db)
-        .await
-        .map_err(|err| {
-            log::error!("Failure when fetching a User: {:?} ({})", login, err,);
+        let user = User::find()
+            .filter(user::Column::Username.eq(&login))
+            .one(&app_state.db)
+            .await
+            .map_err(DbErr::into_api_error)?;
 
-            GetSessionKeyError::ServerError
-        })?;
+        match user {
+            Some(user) => {
+                if user.password == password {
+                    let key = SessionKey::new(user.id, KEY);
 
-    match user {
-        Some(user) => {
-            if user.password == password {
+                    Ok(GetSessionKeyResponse::ExistingUser(key))
+                } else {
+                    Err(APIError::Err(GetSessionKeyError::UserAlreadyExists))
+                }
+            }
+            None => {
+                let user = user::ActiveModel {
+                    username: Set(login),
+                    password: Set(password),
+                    banned: Set(false),
+                    ..Default::default()
+                };
+
+                let user = user.insert(&app_state.db).await.map_err(|err| match err {
+                    DbErr::RecordNotInserted => APIError::Err(GetSessionKeyError::UserAlreadyExists),
+                    _ => err.into_api_error()
+                })?;
+
                 let key = SessionKey::new(user.id, KEY);
 
-                Ok(GetSessionKeyResponse::ExistingUser(key))
-            } else {
-                Err(GetSessionKeyError::UserAlreadyExists)
+                Ok(GetSessionKeyResponse::NewUser(key))
             }
-        }
-        None => {
-            let user = user::ActiveModel {
-                username: Set(login),
-                password: Set(password),
-                banned: Set(false),
-                ..Default::default()
-            };
-
-            let user = user.insert(&state.db).await.map_err(|err| match err {
-                DbErr::RecordNotInserted => GetSessionKeyError::UserAlreadyExists,
-                _ => {
-                    log::error!("Error while inserting: {err}");
-
-                    GetSessionKeyError::ServerError
-                }
-            })?;
-
-            let key = SessionKey::new(user.id, KEY);
-
-            Ok(GetSessionKeyResponse::NewUser(key))
         }
     }
 }
@@ -103,7 +98,9 @@ impl RPCServer for Login {
             state.user = Some(user);
         }
 
-        app_state.connected_clients.insert(user_id, connection_state);
+        app_state
+            .connected_clients
+            .insert(user_id, connection_state);
 
         Ok(())
     }
@@ -121,9 +118,6 @@ impl RPCServer for GetCurrentUser {
     }
 }
 
-pub fn merge(router: RpcRouter<AppState, ConnectionState>) -> RpcRouter<AppState, ConnectionState> {
-    let router = router
-        .register("GetSessionKey", get_session_key);
-
-    register_endpoints!(router, GetCurrentUser, Login)
+pub fn merge(router: GlobalRouter) -> GlobalRouter {
+    register_endpoints!(router, Login, GetCurrentUser, GetSessionKey)
 }
