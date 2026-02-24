372c72869812ac20252a703e952e473719c47cff
Denis Fomin <fo.denis2011@gmail.com>
2026-02-24 20:05:06 +0700

    audio: implement nose reduction

--- crates/capture/src/audio/noise.rs
+++ crates/capture/src/audio/noise.rs
@@ -5,6 +5,8 @@ use rnnoise_sys::{DenoiseState, rnnoise_create, rnnoise_destroy, rnnoise_process
 use crate::audio::VecDequeExt;
 
 const RNN_FRAME_SIZE: usize = 480;
+const SCALE_UP: f32 = 32768.0;
+const SCALE_DOWN: f32 = 1.0 / SCALE_UP;
 
 pub struct RNNoiseState {
     denoise_state: *mut DenoiseState,
@@ -36,11 +38,20 @@ impl RNNoiseState {
 
         while self.input_queue.pop_slice(&mut self.buffer, false) > 0 {
             unsafe {
+                // RNNoise expects samples in int16 range [-32768, 32768]
+                for s in self.buffer.iter_mut() {
+                    *s *= SCALE_UP;
+                }
+
                 let _ = rnnoise_process_frame(
                     self.denoise_state,
                     self.buffer.as_mut_ptr(),
                     self.buffer.as_ptr(),
                 );
+
+                for s in self.buffer.iter_mut() {
+                    *s *= SCALE_DOWN;
+                }
 
                 self.output_queue.extend(self.buffer);
             }
