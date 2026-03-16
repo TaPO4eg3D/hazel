52f92e88073ebe8b3cc36dafbd1968a9e0760e2f
Denis Fomin <fo.denis2011@gmail.com>
2026-03-16 13:23:20 +0700

    vaapi_decode: make the decode just a lil safer

--- crates/capture/src/video/decode.rs
+++ crates/capture/src/video/decode.rs
@@ -1,12 +1,16 @@
 use std::{collections::VecDeque, ffi::c_int};
 
 use drm_fourcc::DrmFourcc;
-use ffmpeg_next::ffi::{
-    av_frame_alloc, av_frame_free, av_frame_unref, av_hwframe_map, av_packet_alloc,
-    av_packet_free, av_parser_close, av_parser_init, av_parser_parse2, avcodec_alloc_context3,
-    avcodec_find_decoder, avcodec_free_context, avcodec_open2, avcodec_receive_frame,
-    avcodec_send_packet, AVCodecContext, AVCodecParserContext, AVDRMFrameDescriptor, AVFrame,
-    AVPacket, AVPixelFormat, AV_HWFRAME_MAP_DIRECT, AV_HWFRAME_MAP_READ, EAGAIN,
+use ffmpeg_next::{
+    Frame, codec, decoder,
+    ffi::{
+        AV_HWFRAME_MAP_DIRECT, AV_HWFRAME_MAP_READ, AVCodecContext, AVCodecParserContext,
+        AVDRMFrameDescriptor, AVFrame, AVPacket, AVPixelFormat, EAGAIN, av_frame_alloc,
+        av_frame_free, av_frame_unref, av_hwframe_map, av_packet_alloc, av_packet_free,
+        av_parser_close, av_parser_init, av_parser_parse2, avcodec_alloc_context3,
+        avcodec_find_decoder, avcodec_free_context, avcodec_open2, avcodec_receive_frame,
+        avcodec_send_packet,
+    },
 };
 
 use crate::video::wrapper::{DrmPlane, GPUDevice};
@@ -46,12 +50,14 @@ pub struct VAAPIDecoderParams {
 }
 
 pub struct VAAPIDecoder {
-    decoder: *mut AVCodecContext,
-    parser: *mut AVCodecParserContext,
     _device: GPUDevice,
 
-    hw_frame: *mut AVFrame,
-    drm_frame: *mut AVFrame,
+    decoder: decoder::Video,
+    parser: *mut AVCodecParserContext,
+
+    hw_frame: Frame,
+    drm_frame: Frame,
+
     packet: *mut AVPacket,
 
     pub frame_queue: VecDeque<DecodedFrame>,
@@ -60,10 +66,7 @@ pub struct VAAPIDecoder {
 impl Drop for VAAPIDecoder {
     fn drop(&mut self) {
         unsafe {
-            avcodec_free_context(&raw mut self.decoder);
             av_parser_close(self.parser);
-            av_frame_free(&raw mut self.hw_frame);
-            av_frame_free(&raw mut self.drm_frame);
             av_packet_free(&raw mut self.packet);
         }
     }
@@ -71,42 +74,37 @@ impl Drop for VAAPIDecoder {
 
 impl VAAPIDecoder {
     pub fn new(params: VAAPIDecoderParams) -> Self {
+        let codec = decoder::find(codec::Id::H264).unwrap();
+
+        let mut decoder = codec::Context::new_with_codec(codec).decoder();
+        let device = GPUDevice::new().expect("Failed to open GPU Device");
+
         unsafe {
-            let codec = avcodec_find_decoder(ffmpeg_next::ffi::AVCodecID::AV_CODEC_ID_H264);
-            assert!(!codec.is_null(), "Failed to find H.264 decoder");
+            let ctx = decoder.as_mut_ptr();
 
-            let ctx = avcodec_alloc_context3(codec);
-            assert!(!ctx.is_null(), "Failed to allocate decoder context");
-
-            let device = GPUDevice::new().expect("Failed to open GPU Device");
             (*ctx).hw_device_ctx = device.clone().into_raw();
-            (*ctx).get_format = Some(vaapi_get_format);
             (*ctx).width = params.width as i32;
             (*ctx).height = params.height as i32;
 
-            let err = avcodec_open2(ctx, codec, std::ptr::null_mut());
-            assert!(err >= 0, "Failed to open H.264 decoder: {err}");
+            (*ctx).get_format = Some(vaapi_get_format);
+        }
 
-            let parser = av_parser_init(
-                ffmpeg_next::ffi::AVCodecID::AV_CODEC_ID_H264 as c_int,
-            );
+        let decoder = decoder.video().expect("Failed to open the decoder");
+
+        unsafe {
+            let parser = av_parser_init(codec::Id::H264 as i32);
             assert!(!parser.is_null(), "Failed to init H.264 parser");
-
-            let hw_frame = av_frame_alloc();
-            assert!(!hw_frame.is_null(), "Failed to allocate hw_frame");
-
-            let drm_frame = av_frame_alloc();
-            assert!(!drm_frame.is_null(), "Failed to allocate drm_frame");
 
             let packet = av_packet_alloc();
             assert!(!packet.is_null(), "Failed to allocate packet");
 
             Self {
-                decoder: ctx,
-                parser,
                 _device: device,
-                hw_frame,
-                drm_frame,
+
+                decoder,
+                parser,
+                hw_frame: Frame::empty(),
+                drm_frame: Frame::empty(),
                 packet,
                 frame_queue: VecDeque::new(),
             }
@@ -123,7 +121,7 @@ impl VAAPIDecoder {
 
                 let consumed = av_parser_parse2(
                     self.parser,
-                    self.decoder,
+                    self.decoder.as_mut_ptr(),
                     &raw mut poutbuf,
                     &raw mut poutbuf_size,
                     data.as_ptr().add(offset),
@@ -140,7 +138,7 @@ impl VAAPIDecoder {
                     (*self.packet).data = poutbuf;
                     (*self.packet).size = poutbuf_size;
 
-                    let err = avcodec_send_packet(self.decoder, self.packet);
+                    let err = avcodec_send_packet(self.decoder.as_mut_ptr(), self.packet);
                     if err < 0 {
                         panic!("Failed to send packet to decoder: {err}");
                     }
@@ -154,27 +152,28 @@ impl VAAPIDecoder {
     unsafe fn receive_frames(&mut self) {
         unsafe {
             loop {
-                let ret = avcodec_receive_frame(self.decoder, self.hw_frame);
+                let ret =
+                    avcodec_receive_frame(self.decoder.as_mut_ptr(), self.hw_frame.as_mut_ptr());
                 if ret == -EAGAIN || ret == -(ffmpeg_next::ffi::AVERROR_EOF as i32) {
                     break;
                 }
                 assert!(ret >= 0, "Decoder error: {ret}");
 
-                let pts = (*self.hw_frame).pts;
-
                 // Map VAAPI surface to DRM PRIME (zero-copy), reusing drm_frame
-                av_frame_unref(self.drm_frame);
-                (*self.drm_frame).format = AVPixelFormat::AV_PIX_FMT_DRM_PRIME as i32;
+                av_frame_unref(self.drm_frame.as_mut_ptr());
+                (*self.drm_frame.as_mut_ptr()).format = AVPixelFormat::AV_PIX_FMT_DRM_PRIME as i32;
 
                 let flags = AV_HWFRAME_MAP_READ as i32 | AV_HWFRAME_MAP_DIRECT as i32;
-                let err = av_hwframe_map(self.drm_frame, self.hw_frame, flags);
+                let err =
+                    av_hwframe_map(self.drm_frame.as_mut_ptr(), self.hw_frame.as_ptr(), flags);
                 if err < 0 {
-                    av_frame_unref(self.hw_frame);
-                    panic!("Failed to map VAAPI frame to DRM PRIME: {err}");
+                    av_frame_unref(self.hw_frame.as_mut_ptr());
+
+                    panic!("Failed to map VAAPI frame to DRM frame: {err}");
                 }
 
                 // Extract DRM descriptor
-                let desc = (*self.drm_frame).data[0] as *const AVDRMFrameDescriptor;
+                let desc = (*self.drm_frame.as_mut_ptr()).data[0] as *const AVDRMFrameDescriptor;
                 assert!(!desc.is_null(), "DRM descriptor is null");
                 assert!((*desc).nb_objects > 0, "No DRM objects");
 
@@ -194,15 +193,16 @@ impl VAAPIDecoder {
 
                 let decoded = DecodedFrame {
                     fd,
-                    width: (*self.drm_frame).width,
-                    height: (*self.drm_frame).height,
+                    width: (*self.drm_frame.as_ptr()).width,
+                    height: (*self.drm_frame.as_ptr()).height,
                     format,
                     modifier,
                     planes,
-                    pts,
+                    pts: self.hw_frame.pts()
+                        .unwrap_or(-1),
                 };
 
-                av_frame_unref(self.hw_frame);
+                av_frame_unref(self.hw_frame.as_mut_ptr());
                 self.frame_queue.push_back(decoded);
             }
         }
